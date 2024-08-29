a28155352a0a5d055b05ad79f22781af5e452c32
Vortex1335 <159593036+Vortex1335@users.noreply.github.com>
2024-08-29 15:50:36 -0700

    Update pw.js

--- pw.js
+++ pw.js
@@ -1,8 +1,7 @@
 var password = prompt("Enter Passcode");
 const an = "1234"
 if (password == an) {
- window.location.replace("https://chrysalis2.vercel.app/mainpg.html/");
- window.location.href = "https://chrysalis2.vercel.app/mainpg.html/";
+
 } else {
   window.close();
 }
