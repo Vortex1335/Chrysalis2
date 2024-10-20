752ce61acb16ef797ab352ef748eed1d2a94c893
Vortex1335 <159593036+Vortex1335@users.noreply.github.com>
2024-10-19 19:23:42 -0700

    Delete pw.js

--- pw.js
+++
@@ -1,7 +0,0 @@
-var password = prompt("Enter Passcode");
-const an = "4t57"
-if (password == an) {
-//window.location.href=("https://chrysalis2.vercel.app/mainpg.html")
-} else {
-  window.location.href=("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-}
